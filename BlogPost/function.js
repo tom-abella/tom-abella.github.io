9a4c663995277ed34e09fd60001f43c4120781df
Tom Leonard Abella <tomleonardabella@gmail.com>
2022-09-28 18:06:29 +0800

    Working API

---
+++ BlogPost/function.js
@@ -0,0 +1,15 @@
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <hr>
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
